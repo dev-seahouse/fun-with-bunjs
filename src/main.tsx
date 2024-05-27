309de9be0b399eb8ebb3f31dc3f86c8004093e3d
Kenan Xin <kenan@bambu.co>
2024-05-27 17:17:11 +0800

    test: test commit hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+
+let root = document.getElementById("root");
+
+if (!root) {
+	root = document.createElement("div");
+}
+
+ReactDOM.createRoot(root).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
